c129253b1c5d4516d376c980e7d2bd3589a77fe5
jessi25924 <jessievirtucio>
2025-05-23 22:32:48 +0100

    add log out unsuccessful, will try again later

--- booking_table/urls.py
+++ booking_table/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('login/', CustomLoginView.as_view(), name='login'),
-    # I’ll add login, dashboard, etc., here later
 ]
