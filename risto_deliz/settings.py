76531bbdaa2dfd280778a4908a89ae7f3d7f7af3
jessi25924 <jessievirtucio>
2025-05-23 10:54:35 +0100

    Modify Secret Key

--- risto_deliz/settings.py
+++ risto_deliz/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nnyh^^ohadcn-d=g!f14gsee&yxq%j@n87)gj53&^9-u@veh!a'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
