ed76941af9e6f90d4ce06127dbfe963d906647ce
jessi25924 <jessievirtucio>
2025-05-24 12:02:31 +0100

    Add Table Model

--- booking_table/models.py
+++ booking_table/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django import settings
 
 # Create your models here.
+class Table(models.Model):
+    """
+    Implement the restaurant table structure.
+
+    Attributes:
+    table_number: An identifier assigned to each table.
+    seating_capacity: Maximum occupancy
+    """
+    table_number = models.PositiveIntegerField(unique=True)
+    seating_capacity = models.PositiveIntegerField()
+
+    def __str__(self):
+        """
+        provides a user-friendly display, e.g. "Table 1 (Seats: 2)".
+        """
+        return f"Table {self.table_number} (Seat: {self.seating_capacity})"
