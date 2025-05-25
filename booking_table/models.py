e601e372890987cfee5c1d344cbddd388799c4bb
jessi25924 <jessievirtucio>
2025-05-25 20:14:46 +0100

    Add MenuItem and migrate

--- booking_table/models.py
+++ booking_table/models.py
@@ -75,3 +75,30 @@ class Booking(models.Model):
             f"{self.user.username} - {self.table} on {self.date} at {self.time}"
             f"({self.guest_count} guests)"
         )
+    
+
+class MenuItem(models.Model):
+    """
+    Represent a dish on the restaurant menu, categorised by course.
+
+    Attributes:
+     name: The name of the dish.
+     description: Brief info about the dish.
+     price: the cost of the dish.
+     category: Tyep of dish (Starter, Pasta, etc.)
+    """
+    CATEGORY_CHOICES = [
+        ('Starter', 'Starter'),
+        ('Pasta', 'Pasta'),
+        ('Pizza', 'Pizza'),
+        ('Dessert', 'Dessert'),
+        ('Drinks', 'Drinks'),
+    ]
+
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
+
+    def __str__(self):
+        return f"{self.name} ({self.category})"
