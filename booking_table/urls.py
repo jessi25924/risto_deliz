3d3019813c2dbf7019f312bffce5fd3c048848de
jessi25924 <jessievirtucio>
2025-05-29 12:25:15 +0100

    cancel booking confirmation still not showing

--- booking_table/urls.py
+++ booking_table/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('login/', CustomLoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(template_name='booking_table/logout.html'), name='logout'),
+    path('logout/', LogoutView.as_view(template_name='registrations/logged_out.html'), name='logout'),
     path('book/', views.book_table, name='book_table'),
     path('booking/<int:pk>/edit/', views.edit_booking, name='edit_booking'),
     path('booking/<int:pk>/cancel/', views.cancel_booking, name='cancel_booking'),
--- risto_deliz/settings.py
+++ risto_deliz/settings.py
@@ -136,9 +136,9 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-LOGIN_URL = 'login'
-LOGIN_REDIRECT_URL = 'dashboard'
-LOGOUT_REDIRECT_URL = 'login'
+# LOGIN_URL = 'login'
+# LOGIN_REDIRECT_URL = 'dashboard'
+# LOGOUT_REDIRECT_URL = 'login'
 
 LOGIN_REDIRECT_URL = '/dashboard/'
 LOGOUT_REDIRECT_URL = '/login/'
