3194ff45e5d51d2dde1478b926859ecff05e64ec
jessi25924 <jessievirtucio>
2025-06-07 10:19:17 +0100

    Refine signup page styling

--- booking_table/forms.py
+++ booking_table/forms.py
@@ -6,13 +6,22 @@ from .models import Table
 from django.core.exceptions import ValidationError
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=50, required=True, help_text='Your given name')
-    last_name  = forms.CharField(max_length=50, required=True, help_text='Your family name')
-    email      = forms.EmailField(max_length=254, required=True, help_text='Enter a valid email address')
-
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'username': 'username',
+            'first_name': 'first name',
+            'last_name': 'last name',
+            'email': 'email',
+            'password1': 'password',
+            'password2': 'confirm password',
+        }
+        for field_name, field in self.fields.items():
+            field.widget.attrs['placeholder'] = placeholders.get(field_name, '')
 
 
 class BookingForm(forms.ModelForm):
